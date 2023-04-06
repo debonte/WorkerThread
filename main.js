b39ba05eb724feca7245b934487bfd1e06b476fb
Erik De Bonte <erikd@microsoft.com>
2023-04-06 11:52:06 -0700

    Loop sending messages; try to launch without debugging

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 const { Worker } = require('worker_threads');
 
-// create a new worker thread
-const worker = new Worker('./worker.js');
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-// send a message to the worker thread
-worker.postMessage('Hello from the main thread!');
+async function run() {
+  async function main () {
+    // create a new worker thread
+    const worker = new Worker('./worker.js');
 
-// listen for messages from the worker thread
-worker.on('message', (message) => {
-  console.log('Message received from worker thread:', message);
-});
+    // listen for messages from the worker thread
+    worker.on('message', (message) => {
+      console.log('Message received from worker thread:', message);
+    });
+
+    while (true){
+      // send a message to the worker thread
+      worker.postMessage('Hello from the main thread!');
+
+      await sleep(3000);
+    }
+  }
+
+  await main();
+}
+
+run();
